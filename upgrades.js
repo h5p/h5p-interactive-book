703aa6aa03e3919474583a03b0a7bbade6464806
Vilde Stabell <vilde.voss.stabell@gmail.com>
2024-01-19 18:19:20 +0100

    JI-5423 Upgrade CKE4 tables border styles

--- upgrades.js
+++ upgrades.js
@@ -92,6 +92,61 @@ H5PUpgrades['H5P.InteractiveBook'] = (function () {
         }
 
         finished(null, parameters, extras);
+      },
+      8: function (parameters, finished, extras) {
+        const tables = parameters.bookCover.coverDescription.split('<table');
+        let newParams = tables[0];
+
+        for (let i = 1; i < tables.length; i++) {
+          const style = tables[i].includes('h5p-table') ? 
+            'style="border-style:solid;' :
+            'style="border-style:double;border-width:0.2em;border-collapse:collapse;';
+
+          if (tables[i].includes('border')) {
+            // Set border style on the table
+            if (tables.includes('style="')) {
+              tables[i] = tables[i].replace('style="', style);
+            }
+            else {
+              tables[i] = ' ' + style + '"' + tables[i];
+            }
+
+            // Set border style on header cells
+            let headers = tables[i].split(/<th(?!ead)/);
+            tables[i] = headers[0];
+
+            for (let j = 1; j < headers.length; j++) {
+              tables[i] += '<th';
+
+              if (headers[j].includes('style="')) {
+                tables[i] += headers[j].replace('style="', style);
+              }
+              else {
+                tables[i] += ' ' + style + '"' + headers[j];
+              }
+            }
+            
+            // Set border style on cells
+            let cells = tables[i].split('<td');
+            tables[i] = cells[0];
+
+            for (let j = 1; j < cells.length; j++) {
+              tables[i] += '<td';
+
+              if (cells[j].includes('style="')) {
+                tables[i] += cells[j].replace('style="', style);
+              }
+              else {
+                tables[i] += ' ' + style + '"' + cells[j];
+              }
+            }
+          }
+
+          newParams += '<table' + tables[i];
+        }
+
+        parameters.bookCover.coverDescription = newParams;
+        finished(null, parameters, extras);
       }
     }
   };
