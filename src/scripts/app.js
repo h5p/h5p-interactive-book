f00c6f13baca047e34547cc26d223fd2aeab908a
Vilde Voss Stabell <Vilde.voss.stabell@gmail.com>
2024-10-07 14:04:50 +0200

    LB-140 Interactions when rows (#164)
    * LB-140 Unpack content from rows
    
    * LB-140 Fix section names in sidebar

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -311,7 +311,7 @@ export default class InteractiveBook extends H5P.EventDispatcher {
      * @param {Array} chapters
      * @return {boolean}
      */
-    this.hasChaptersTasks = chapters => chapters.filter(chapter => chapter.sections.filter(section => (section.isTask === true || (section.content.metadata.contentType === 'Row'))).length > 0).length > 0;
+    this.hasChaptersTasks = chapters => chapters.filter(chapter => chapter.sections.filter(section => section.isTask === true).length > 0).length > 0;
 
     /**
      * Check if there are valid chapters.
@@ -969,7 +969,7 @@ export default class InteractiveBook extends H5P.EventDispatcher {
     });
     this.chapters = this.pageContent.getChapters();
 
-    this.sideBar = new SideBar(this.params, contentId, contentData.metadata.title, this);
+    this.sideBar = new SideBar(this.params, contentId, contentData.metadata.title, this, this.chapters);
 
     // Set progress (from previous state);
     this.chapters.forEach((chapter, index) => {
